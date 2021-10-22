abc83260e03d27b5fb36d25123df16da47963f24
ram173 <chaurasia4u@gmail.com>
2021-10-22 10:33:46 +0530

    update

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,15 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import AliceCarousel from 'react-alice-carousel';
+import AliceCarousel from "react-alice-carousel";
 export default class Footer extends React.Component {
   state = {
     footerItems: [
-      <div className="swiper-slide"><img src={require("../public/india-gov.webp")} className="img-fluid" alt="India gov" /></div>,
-      <div className="swiper-slide"><img src={require("../public/mot.webp")} className="img-fluid" alt="Ministry Textile" /></div >,
-      <div className="swiper-slide"><img src={require("../public/di.webp")} className="img-fluid" alt="DIC" /></div >,
-      <div className="swiper-slide"><img src={require("../public/india_post.webp")} className="img-fluid" alt="India Post" /></div >,
-      <div className="swiper-slide"> <img src={require("../public/csc.webp")} className="img-fluid" alt="CSC" /></div>,
-      <div className="swiper-slide"> <img src={require("../public/make-in-india.webp")} className="img-fluid" alt="make in india" /></div>],
+      <div className="swiper-slide">
+        <img
+          src={require("../public/india-gov.webp")}
+          className="img-fluid"
+          alt="India gov"
+        />
+      </div>,
+      <div className="swiper-slide">
+        <img
+          src={require("../public/mot.webp")}
+          className="img-fluid"
+          alt="Ministry Textile"
+        />
+      </div>,
+      <div className="swiper-slide">
+        <img
+          src={require("../public/di.webp")}
+          className="img-fluid"
+          alt="DIC"
+        />
+      </div>,
+      <div className="swiper-slide">
+        <img
+          src={require("../public/india_post.webp")}
+          className="img-fluid"
+          alt="India Post"
+        />
+      </div>,
+      <div className="swiper-slide">
+        {" "}
+        <img
+          src={require("../public/csc.webp")}
+          className="img-fluid"
+          alt="CSC"
+        />
+      </div>,
+      <div className="swiper-slide">
+        {" "}
+        <img
+          src={require("../public/make-in-india.webp")}
+          className="img-fluid"
+          alt="make in india"
+        />
+      </div>,
+    ],
     responsive: {
       0: { items: 2 },
       568: { items: 3 },
@@ -31,8 +70,12 @@ export default class Footer extends React.Component {
                       <h5>Information</h5>
                       <div className="footer-links">
                         <ul>
-                          <li><Link to="/terms-of-use">Terms of use</Link></li>
-                          <li><Link to="/seller-policy">Seller Policy </Link></li>
+                          <li>
+                            <Link to="/terms-of-use">Terms of use</Link>
+                          </li>
+                          <li>
+                            <Link to="/seller-policy">Seller Policy </Link>
+                          </li>
                           {/* <li><Link to="/">Right to Information</Link></li>
                           <li><Link to="/">Sitemap </Link></li>
                           <li><Link to="/">Subscribe Newsletter</Link></li> */}
@@ -43,10 +86,18 @@ export default class Footer extends React.Component {
                       <h5>Resources</h5>
                       <div className="footer-links">
                         <ul>
-                          <li><Link to="/terms-and-condition">Terms &amp; Conditions</Link></li>
-                          <li><Link to="/buyer-policy">Buyer Policy</Link></li>
+                          <li>
+                            <Link to="/terms-and-condition">
+                              Terms &amp; Conditions
+                            </Link>
+                          </li>
+                          <li>
+                            <Link to="/buyer-policy">Buyer Policy</Link>
+                          </li>
 
-                          <li><Link to="/exchange-policy">Exchange Policy</Link></li>
+                          <li>
+                            <Link to="/exchange-policy">Exchange Policy</Link>
+                          </li>
                         </ul>
                       </div>
                     </div>
@@ -54,9 +105,22 @@ export default class Footer extends React.Component {
                       <h5>Partner with us</h5>
                       <div className="footer-links">
                         <ul>
-                          <li><a href="https://seller.digitalindiacorporation.in/register"> Weaver / Artisan Registration</a></li>
-                          <li><a href="https://seller.digitalindiacorporation.in/login">Weaver / Artisan Login</a></li>
-                          <li><Link to="/corporate-enquiries">Corporate Enquiries </Link></li>
+                          <li>
+                            <a href="https://seller.digitalindiacorporation.in/register">
+                              {" "}
+                              Weaver / Artisan Registration
+                            </a>
+                          </li>
+                          <li>
+                            <a href="https://seller.digitalindiacorporation.in/login">
+                              Weaver / Artisan Login
+                            </a>
+                          </li>
+                          <li>
+                            <Link to="/corporate-enquiries">
+                              Corporate Enquiries{" "}
+                            </Link>
+                          </li>
                         </ul>
                       </div>
                     </div>
@@ -64,9 +128,15 @@ export default class Footer extends React.Component {
                       <h5>Need Help?</h5>
                       <div className="footer-links">
                         <ul>
-                          <li><Link to="/faq">FAQs</Link></li>
-                          <li><Link to="/my-account/order">Raise a ticket</Link></li>
-                          <li><Link to="/contact-us">Contact Us </Link></li>
+                          <li>
+                            <Link to="/faq">FAQs</Link>
+                          </li>
+                          <li>
+                            <Link to="/my-account/order">Raise a ticket</Link>
+                          </li>
+                          <li>
+                            <Link to="/contact-us">Contact Us </Link>
+                          </li>
                         </ul>
                       </div>
                     </div>
@@ -75,14 +145,20 @@ export default class Footer extends React.Component {
                 <div className="col-md-4 col-lg-4 col-12">
                   <h5>We Accept</h5>
                   <div className="payment_method">
-                    <div className="paycard"><img src={require("../public/pm.webp")} className="img-fluid" alt="pay" /></div>
+                    <div className="paycard">
+                      <img
+                        src={require("../public/pm.webp")}
+                        className="img-fluid"
+                        alt="pay"
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
 
               <div className="py-5 logos">
                 <AliceCarousel
-                  animationType='slide'
+                  animationType="slide"
                   autoPlayInterval={3000}
                   autoPlay={true}
                   disableDotsControls
@@ -92,26 +168,45 @@ export default class Footer extends React.Component {
                   mouseTracking
                   infinite
                   disableButtonsControls //can be enabled if arrows are needed
-                //touchTracking
+                  //touchTracking
                 />
-
               </div>
             </div>
           </div>
           <div className="copyright-footer">
             <div className="container-fluid">
               <div className="row">
-                <div className="col-md-6 col-12"> <small>Ministry of Textile
-                  <i className="fa fa-copyright"></i> 2020. <br />
-                  Designed &amp; Developed by Digital India Corporation</small>
+                <div className="col-md-6 col-12">
+                  {" "}
+                  <small>
+                    Ministry of Rural Development
+                    <i className="fa fa-copyright" /> 2020. <br />
+                    Designed &amp; Developed by Digital India Corporation
+                  </small>
                 </div>
-                <div className="col-md-6 col-12 text-right"> <a href="#"><img src={require("../public/gp-300x89.png")}
-                  className="img-fluid" alt="google play store" /></a> &nbsp;&nbsp; <a href="#"><img
-                    src={require("../public/as-300x89.png")} className="img-fluid" alt="itue" /></a> </div>
+                <div className="col-md-6 col-12 text-right">
+                  {" "}
+                  <a href="#">
+                    <img
+                      src={require("../public/gp-300x89.png")}
+                      className="img-fluid"
+                      alt="google play store"
+                    />
+                  </a>{" "}
+                  &nbsp;&nbsp;{" "}
+                  <a href="#">
+                    <img
+                      src={require("../public/as-300x89.png")}
+                      className="img-fluid"
+                      alt="itue"
+                    />
+                  </a>{" "}
+                </div>
               </div>
             </div>
           </div>
         </footer>
-      </>);
+      </>
+    );
   }
 }
--- src/component/ShopByType.js
+++ src/component/ShopByType.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import AliceCarousel from 'react-alice-carousel';
-import CategoryService from '../services/CategoryService';
-import ProductService from '../services/ProductService';
-import { connect } from 'react-redux';
-import * as wishlistAction from '../actions/wishlist';
-import * as compareAction from '../actions/compare';
-import * as cartAction from '../actions/cart';
-import ProductTile from './ProductTile';
-import ToastService from '../services/ToastService';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import AliceCarousel from "react-alice-carousel";
+import CategoryService from "../services/CategoryService";
+import ProductService from "../services/ProductService";
+import { connect } from "react-redux";
+import * as wishlistAction from "../actions/wishlist";
+import * as compareAction from "../actions/compare";
+import * as cartAction from "../actions/cart";
+import ProductTile from "./ProductTile";
+import ToastService from "../services/ToastService";
 class ShopByType extends Component {
   constructor(props) {
     super(props);
@@ -23,117 +23,138 @@ class ShopByType extends Component {
         1024: { items: 5 },
       },
       shopByCategoryItems: [],
-      shopByProductItems: []
+      shopByProductItems: [],
     };
   }
 
   componentDidUpdate(prevProps) {
     if (this.props.tabType !== prevProps.tabType) {
-      this.getProductsList(this.getSetQueryParams())
+      this.getProductsList(this.getSetQueryParams());
     }
   }
 
   componentDidMount() {
-    this.state.type === 'product' ? this.getProductsList(this.getSetQueryParams()) : this.getCateroryList();
+    this.state.type === "product"
+      ? this.getProductsList(this.getSetQueryParams())
+      : this.getCateroryList();
   }
   getSetQueryParams() {
     let queryParams = {};
     if (this.props.tabType) {
-      queryParams = this.props.tabType === 2 ? { 'order_by': 'desc', 'sort_by': 'created_at' } : {}
+      queryParams =
+        this.props.tabType === 2
+          ? { order_by: "desc", sort_by: "created_at" }
+          : {};
     }
     if (this.props.catId) {
-      queryParams = { cat_ids: [this.props.catId] }
+      queryParams = { cat_ids: [this.props.catId] };
     }
     return queryParams;
   }
 
   getCateroryList = () => {
-    let catImages = [{
-      icon: require('../public/icons/eshilp-mens-wear.svg'),
-      backgroundImage: `url( https://app.digitalindiacorporation.in/v1/digi/wp-content/uploads/2020/11/mens_wear-300x300.webp)`
-    },
-    {
-      icon: require('../public/icons/eshilp-women-wears.svg'),
-      backgroundImage: `url( "https://app.digitalindiacorporation.in/v1/digi/wp-content/uploads/2020/11/womens_wear-300x300.webp")`
-    },
-    {
-      icon: require('../public/icons/eshilp-home-decore.svg'),
-      backgroundImage: `url( "https://app.digitalindiacorporation.in/v1/digi/wp-content/uploads/2020/11/decor-300x300.webp")`
-    },
-    {
-      icon: require('../public/icons/eshilp-furniture.svg'),
-      backgroundImage: `url( 'https://app.digitalindiacorporation.in/v1/digi/wp-content/uploads/2020/11/womens_wear-300x300.webp')`
-    },
-    {
-      icon: require('../public/icons/eshilp-floor-covering.svg'),
-      backgroundImage: '../public/floorcovering.jpg'
-    },
-    {
-      icon: require('../public/icons/eshilp-travel-ass.svg'),
-      backgroundImage: `url( require('../public/clothing_accessories.jpg'))`
-    },
-    {
-      icon: require('../public/icons/eshilp-office-supplies.svg'),
-      backgroundImage: `url( require('../public/officeSupplies.jpeg'))`
-    }
-
+    let catImages = [      
+      {
+        icon: require("../public/icons/eshilp-floor-covering.svg"),
+        bgImage: require("../public/floorcovering.jpg"),
+      },
+      {
+        icon: require("../public/icons/eshilp-travel-ass.svg"),
+        bgImage: require("../public/clothing_accessories.jpg"),
+      },
+      {
+        icon: require("../public/icons/eshilp-office-supplies.svg"),
+        bgImage: require("../public/officeSupplies.jpg"),
+      },
     ];
-    CategoryService.fetchAllCategory({ parent_id: 0 }).then((result) => {
+    CategoryService.fetchAllCategory({ parent_id: 0 }).then(result => {
       this.setState({
-        shopByCategoryItems: result?.map((item, index) =>
-        (<div className="flip-container" key={index} >
-          <div className="categorie-img" onClick={() => this.productList(item.id)}>
-            {/* <div className="cat-overlay"></div> */}
-            <div className="categorie-img-front" style={{ backgroundImage: catImages[index]?.backgroundImage }} >
-
-              <div className="more-products"><Link to="/product-list">225<br />More Products</Link></div>
-
-            </div>
-            <div className="categorie-img-Back" style={{ backgroundImage: catImages[index]?.backgroundImage }}>
-              <div className="categorie-img-title">
-                <div className="cat_icon"><img src={catImages[index]?.icon} className="img-fluid" alt="pay" /></div>
-                <h3>{item.title}</h3>
+        shopByCategoryItems: result?.map((item, index) => (
+          <div className="flip-container" key={index}>
+            <div
+              className="categorie-img"
+              onClick={() => this.productList(item.id)}
+            >
+              {/* <div className="cat-overlay"></div> */}
+              <div
+                className="categorie-img-front"
+                style={{ backgroundImage: catImages[index]?.bgImage }}
+              >
+                <div className="more-products">
+                  <Link to="/product-list">
+                    225
+                    <br />
+                    More Products
+                  </Link>
+                </div>
               </div>
-
+              <div
+                className="categorie-img-Back"
+                style={{ backgroundImage: catImages[index]?.bgImage }}
+              >
+                <div className="categorie-img-title">
+                  <div className="cat_icon">
+                    <img
+                      src={catImages[index]?.icon}
+                      className="img-fluid"
+                      alt="pay"
+                    />
+                  </div>
+                  <h3>{item.title}</h3>
+                </div>
+              </div>
             </div>
-
           </div>
-
-        </div>)
-        )
-      })
+        )),
+      });
     });
-  }
+  };
 
-  getProductsList = (filterparams) => {
-    ProductService.fetchAllProducts(filterparams).then((result) => {
+  getProductsList = filterparams => {
+    ProductService.fetchAllProducts(filterparams).then(result => {
       this.setState({
-        shopByProductItems: result?.data?.map((item) =>
-          (<ProductTile data={item} {...this.props} errorAlert={this.errorAlert} successAlert={this.successAlert} limitAlert={this.limitAlert} />))
-      })
-    })
-  }
-
-  productList = (value) => {
-
-    this.props.history.push({
-      pathname: '/product-list',
-      search: "?cat_ids=" + value
+        shopByProductItems: result?.data?.map(item => (
+          <ProductTile
+            data={item}
+            {...this.props}
+            errorAlert={this.errorAlert}
+            successAlert={this.successAlert}
+            limitAlert={this.limitAlert}
+          />
+        )),
+      });
     });
-  }
+  };
+
+  productList = value => {
+    this.props.history.push({
+      pathname: "/product-list",
+      search: "?cat_ids=" + value,
+    });
+  };
 
   errorAlert = (product, type) => {
-    return ToastService.error(product?.content?.title + " is " +
-      (type === "cart" ? "already in cart" : "removed from wishlist"));
-  }
+    return ToastService.error(
+      product?.content?.title +
+        " is " +
+        (type === "cart" ? "already in cart" : "removed from wishlist"),
+    );
+  };
   successAlert = (product, type) => {
-    return ToastService.success(product?.content?.title + " is successfully added to " + type)
-  }
+    return ToastService.success(
+      product?.content?.title + " is successfully added to " + type,
+    );
+  };
   limitAlert = () => {
-    return ToastService.error("Compare Cart is full(limit :5)")
-  }
+    return ToastService.error("Compare Cart is full(limit :5)");
+  };
   render() {
-    const { type, shopByCategoryItems, shopByProductItems, responsive } = this.state;
+    const {
+      type,
+      shopByCategoryItems,
+      shopByProductItems,
+      responsive,
+    } = this.state;
     return (
       <>
         <AliceCarousel
@@ -141,7 +162,7 @@ class ShopByType extends Component {
           autoPlay={true}
           autoPlayStrategy="all"
           disableDotsControls
-          items={type === 'product' ? shopByProductItems : shopByCategoryItems}
+          items={type === "product" ? shopByProductItems : shopByCategoryItems}
           responsive={responsive}
           mouseTracking
           infinite
@@ -149,8 +170,7 @@ class ShopByType extends Component {
           touchTracking
         />
       </>
-
-    )
+    );
   }
 }
 
@@ -158,18 +178,20 @@ const mapStateToProps = state => {
   return {
     wishlist: state.wishlist,
     userData: state.userData,
-    cart: state.cart
-  }
+    cart: state.cart,
+  };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     addToWishlist: wishlist => dispatch(wishlistAction.addToWishlist(wishlist)),
     deleteWishlist: index => dispatch(wishlistAction.deleteWishlist(index)),
     addToCompare: compare => dispatch(compareAction.addToCompare(compare)),
-    addToCart: cart => dispatch(cartAction.addToCart(cart))
-  }
+    addToCart: cart => dispatch(cartAction.addToCart(cart)),
+  };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(ShopByType);
-
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ShopByType);
