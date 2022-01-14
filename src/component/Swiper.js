d398a22472a63f5519967f1e4a686b43c7506acc
singhaishwarya <aishsinghniit@gmail.com>
2022-01-14 16:28:51 +0530

    swiper image

--- src/component/Swiper.js
+++ src/component/Swiper.js
@@ -13,7 +13,7 @@ export default class Swiper extends React.Component {
     this.setState({
       galleryItems: [
         <img key='0' src={require("../public/slider-1.png")} className="img-fluid" style={{ width: 1395 }} alt="" />,
-        <img key='1' src={require("../public/bannr2.webp")} className="img-fluid" style={{ width: 1395 }} alt="" />,
+        <img key='1' src={require("../public/bannr2.png")} className="img-fluid" style={{ width: 1395 }} alt="" />,
         <img key='2' src={require("../public/bannr3.webp")} className="img-fluid" style={{ width: 1395 }} alt="" />,
         <img key='3' src={require("../public/bannr4.webp")} className="img-fluid" style={{ width: 1395 }} alt="" />]
     });
